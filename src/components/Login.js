266e4c21449ca28ba45e249c8c829e129cf95fcb
Muhammad Zeeshan <muhamadzeeshan03@gmail.com>
2022-09-23 14:18:47 +0500

    proper-routes-added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,36 @@
-import { React, useState } from 'react'
-
+import { React, useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 export default function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const handleLogin = () => {
-    console.log(email, password)
-  }
+  const navigate = useNavigate()
 
+  useEffect(() => {
+    const auth = localStorage.getItem('user')
+    if (auth) {
+      navigate('/')
+    }
+  })
+  const handleLogin = async () => {
+    console.log(email, password)
+
+    let result = await fetch('http://localhost:5000/login', {
+      method: 'post',
+      body: JSON.stringify({ email, password }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    result = await result.json()
+    console.log(result)
+
+    if (result.name) {
+      localStorage.setItem('user', JSON.stringify(result))
+      navigate('/')
+    } else {
+      alert('Please Enter Correct Details')
+    }
+  }
   return (
     <div className="login">
       <input
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,35 +10,43 @@ const Nav = () => {
   }
   return (
     <div>
-      <ul className="nav-ul">
-        <li>
-          <Link to="/"> Home page</Link>
-        </li>
+      <img
+        className="logo"
+        src="https://propakistani.pk/how-to/wp-content/uploads/2020/06/1-27.jpg"
+        alt="logo"
+      />
+      {auth ? (
+        <ul className="nav-ul">
+          <li>
+            <Link to="/"> Home page</Link>
+          </li>
 
-        <li>
-          <Link to="/add"> Add Product</Link>
-        </li>
-        <li>
-          <Link to="/update"> Update Product</Link>
-        </li>
-        <li>
-          <Link to="/Profile"> Profile </Link>
-        </li>
-        {auth ? (
+          <li>
+            <Link to="/add"> Add Product</Link>
+          </li>
+          <li>
+            <Link to="/update"> Update Product</Link>
+          </li>
+          <li>
+            <Link to="/Profile"> Profile </Link>
+          </li>
+
           <li>
             <Link onClick={logout} to="/signup">
-              logout
+              logout ({JSON.parse(auth).name})
             </Link>
           </li>
-        ) : (
+        </ul>
+      ) : (
+        <ul className="nav-ul nav-right">
           <li>
             <Link to="/signup"> Signup </Link>
           </li>
-        )}
-        <li>
-          <Link to="/login"> Login </Link>
-        </li>
-      </ul>
+          <li>
+            <Link to="/login"> Login </Link>
+          </li>
+        </ul>
+      )}
     </div>
   )
 }
