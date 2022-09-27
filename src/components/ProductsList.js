10373e92c8fae5383a7c439d12b3190a009ad60c
Muhammad Zeeshan <muhamadzeeshan03@gmail.com>
2022-09-27 18:01:25 +0500

    search-product-added

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -24,23 +24,47 @@ function ProductsList() {
     }
   }
 
-  const resultAray = products.map((item, index) => (
-    <ul key={item._id}>
-      <li>{index + 1}</li>
-      <li>{item.name}</li>
-      <li>{item.price}</li>
-      <li>{item.category}</li>
-      <li>{item.company}</li>
-      <li>
-        <button onClick={() => deleteProduct(item._id)}>Delete</button>
-        <Link to={'/update/' + item._id}>Update</Link>
-      </li>
-    </ul>
-  ))
+  const searchHandle = async (event) => {
+    console.log(event.target.value)
+    let search = event.target.value
+    if (search) {
+      let result = await fetch(`http://localhost:5000/search/${search}`)
+      result = await result.json()
+      if (result) {
+        setProducts(result)
+      }
+    } else {
+      getProducts()
+    }
+  }
+  const resultAray =
+    products.length > 0 ? (
+      products.map((item, index) => (
+        <ul key={item._id}>
+          <li>{index + 1}</li>
+          <li>{item.name}</li>
+          <li>{item.price}</li>
+          <li>{item.category}</li>
+          <li>{item.company}</li>
+          <li>
+            <button onClick={() => deleteProduct(item._id)}>Delete</button>
+            <Link to={'/update/' + item._id}>Update</Link>
+          </li>
+        </ul>
+      ))
+    ) : (
+      <h1 style={{ color: 'red' }}>No Data Found</h1>
+    )
 
   return (
     <div className="product-list">
       <h3>Product list</h3>
+      <input
+        type="text"
+        className="search_box"
+        placeholder="Search Product"
+        onChange={searchHandle}
+      />
       <ul>
         <li>S. No</li>
         <li>Name</li>
